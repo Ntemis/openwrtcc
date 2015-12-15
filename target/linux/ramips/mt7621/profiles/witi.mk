ee46e80b68822945f1240ad4abec0936443c8290
demetris <ierokipides.dem@gmail.com>
2015-12-15 23:25:25 +0200

    Add MT7621 platform i2c controller driver to WiTi

--- target/linux/ramips/mt7621/profiles/witi.mk
+++ target/linux/ramips/mt7621/profiles/witi.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 OpenWrt.org
+# Copyright (C) 2015 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -7,14 +7,12 @@
 
 define Profile/WITI
 	NAME:=WITI Device
+	FEATURES+=rtc
 	PACKAGES:=\
 		kmod-usb-core kmod-usb3 kmod-usb-hid kmod-sdhci-mt7620 \
 		kmod-ledtrig-usbdev kmod-ata-core kmod-ata-ahci \
-		kmod-usb3-mt7621 kmod-rtc-pcf8563
+		kmod-usb3-mt7621 kmod-rtc-pcf8563 kmod-i2c-mt7621
 endef
-
-
-FEATURES+=rtc
 
 define Profile/WITI/Description
 	Default package set compatible with most boards.
